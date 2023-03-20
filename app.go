e4bef06fa6adaacfa05cea3fc28f0401f5f19b3b
jutionck <jutionck@gmail.com>
2023-03-20 17:42:59 +0700

    add: Go DB Introduction

--- app.go
+++ app.go
@@ -1,12 +1,36 @@
 package main
 
-import "github.com/jutionck/golang-db-sinar-harapan-makmur/delivery"
+import (
+	"database/sql"
+	"fmt"
+	"log"
+
+	"github.com/common-nighthawk/go-figure"
+	_ "github.com/lib/pq"
+)
 
 func main() {
 
-	// delivery.VehicleCLI()
-	// delivery.CustomerCLI()
-	// delivery.EmployeeCLI()
-	delivery.TransactionCLI()
+	host := "localhost"
+	port := "5432"
+	user := "postgres"
+	password := "P@ssw0rd"
+	dbname := "db_sinar_harapan_makmur"
 
+	psqlInfo := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
+	db, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		log.Fatalf("Driver tidak ditemukan : %s", err)
+	}
+
+	defer db.Close()
+
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
+
+	figure := figure.NewFigure("Enigma Camp", "standard", true)
+	figure.Print()
+	fmt.Println("Successfully connected!")
 }
--- go.mod
+++ go.mod
@@ -3,3 +3,9 @@ module github.com/jutionck/golang-db-sinar-harapan-makmur
 go 1.19
 
 require github.com/google/uuid v1.3.0
+
+require (
+	github.com/common-nighthawk/go-figure v0.0.0-20210622060536-734e95fb86be // indirect
+	github.com/jmoiron/sqlx v1.3.5 // indirect
+	github.com/lib/pq v1.10.7 // indirect
+)
